6baaf79c31ba5dced272201eeb75446bb48e8be3
Andrew E. Falcon <aefalcon@gmail.com>
2018-09-09 13:42:30 -0500

    Messages in services should be pointers

--- keyservice/keyservice.go
+++ keyservice/keyservice.go
@@ -5,14 +5,14 @@ import (
 )
 
 type ManagerService interface {
-	AddApp(appkeypb.AddAppRequest) (appkeypb.AddAppResponse, error)
-	RemoveApp(appkeypb.RemoveAppRequest) (appkeypb.RemoveAppResponse, error)
-	GetApp(appkeypb.GetAppRequest) (appkeypb.App, error)
-	ListAps(appkeypb.ListAppsRequest) (appkeypb.AppIndex, error)
-	AddKey(appkeypb.AddKeyRequest) (appkeypb.AddKeyResponse, error)
-	RemoveKey(appkeypb.RemoveKeyRequest) (appkeypb.RemoveKeyResponse, error)
+	AddApp(*appkeypb.AddAppRequest) (*appkeypb.AddAppResponse, error)
+	RemoveApp(*appkeypb.RemoveAppRequest) (*appkeypb.RemoveAppResponse, error)
+	GetApp(*appkeypb.GetAppRequest) (*appkeypb.App, error)
+	ListAps(*appkeypb.ListAppsRequest) (*appkeypb.AppIndex, error)
+	AddKey(*appkeypb.AddKeyRequest) (*appkeypb.AddKeyResponse, error)
+	RemoveKey(*appkeypb.RemoveKeyRequest) (*appkeypb.RemoveKeyResponse, error)
 }
 
 type SigningService interface {
-	Sign(appkeypb.SignRequest) (appkeypb.SignedData, error)
+	Sign(*appkeypb.SignRequest) (*appkeypb.SignedData, error)
 }
