f95bfc6c87caf43b5ee17915578398ddfae073b6
Andrew E. Falcon <aefalcon@gmail.com>
2018-09-09 14:47:37 -0500

    Fix identifier typo

--- keyservice/keyservice.go
+++ keyservice/keyservice.go
@@ -10,7 +10,7 @@ type ManagerService interface {
 	AddApp(*appkeypb.AddAppRequest, *log.Logger) (*appkeypb.AddAppResponse, error)
 	RemoveApp(*appkeypb.RemoveAppRequest, *log.Logger) (*appkeypb.RemoveAppResponse, error)
 	GetApp(*appkeypb.GetAppRequest, *log.Logger) (*appkeypb.App, error)
-	ListAps(*appkeypb.ListAppsRequest, *log.Logger) (*appkeypb.AppIndex, error)
+	ListApps(*appkeypb.ListAppsRequest, *log.Logger) (*appkeypb.AppIndex, error)
 	AddKey(*appkeypb.AddKeyRequest, *log.Logger) (*appkeypb.AddKeyResponse, error)
 	RemoveKey(*appkeypb.RemoveKeyRequest, *log.Logger) (*appkeypb.RemoveKeyResponse, error)
 }
