51de16634d27d966a04c23db745a82a3059a38c9
Andrew E. Falcon <aefalcon@gmail.com>
2018-09-09 13:18:52 -0500

    Add service interfaces

---
+++ keyservice/keyservice.go
@@ -0,0 +1,18 @@
+package keyservice
+
+import (
+	"github.com/aefalcon-redux/github-keystore-protobuf/go/appkeypb"
+)
+
+type ManagerService interface {
+	AddApp(appkeypb.AddAppRequest) (appkeypb.AddAppResponse, error)
+	RemoveApp(appkeypb.RemoveAppRequest) (appkeypb.RemoveAppResponse, error)
+	GetApp(appkeypb.GetAppRequest) (appkeypb.App, error)
+	ListAps(appkeypb.ListAppsRequest) (appkeypb.AppIndex, error)
+	AddKey(appkeypb.AddKeyRequest) (appkeypb.AddKeyResponse, error)
+	RemoveKey(appkeypb.RemoveKeyRequest) (appkeypb.RemoveKeyResponse, error)
+}
+
+type SigningService interface {
+	Sign(appkeypb.SignRequest) (appkeypb.SignedData, error)
+}
