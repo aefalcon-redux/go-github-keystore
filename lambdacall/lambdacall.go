55e0d19a92e41bd13f88fb659c83d49f1f4bb7bf
Andrew E. Falcon <aefalcon@gmail.com>
2018-09-20 14:42:06 +0000

    Add framework for calling lambda services

---
+++ lambdacall/lambdacall.go
@@ -0,0 +1,37 @@
+package lambdacall
+
+import (
+	"github.com/aefalcon/github-keystore-protobuf/go/appkeypb"
+	"github.com/aefalcon/go-github-keystore/kslog"
+	"github.com/aws/aws-sdk-go/service/lambda"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
+)
+
+func CallPbLambda(svc *lambda.Lambda, funcName string, in, out proto.Message) error {
+	marshaler := jsonpb.Marshaler{}
+	reqDoc, err := marshaler.MarshalToString(in)
+	if err != nil {
+		return err
+	}
+	invokeInput := lambda.InvokeInput{
+		FunctionName: &funcName,
+		Payload:      []byte(reqDoc),
+	}
+	invokeOutput, err := svc.Invoke(&invokeInput)
+	if err != nil {
+		return err
+	}
+	return jsonpb.UnmarshalString(string(invokeOutput.Payload), out)
+}
+
+type LambdaSigningService struct {
+	Service  *lambda.Lambda
+	FuncName string
+}
+
+func (s *LambdaSigningService) SignJwt(req *appkeypb.SignJwtRequest, logger kslog.KsLogger) (*appkeypb.SignJwtResponse, error) {
+	var resp appkeypb.SignJwtResponse
+	err := CallPbLambda(s.Service, s.FuncName, req, &resp)
+	return &resp, err
+}
